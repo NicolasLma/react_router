2fc8d360efa6eb750720116c58254a064e937c9a
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-04-15 10:55:26 +0200

    react_router_dom

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <Home />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <About />
+        </main>
+      </>
+    ),
+  },
+]);
+
+// rendering
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
